7db991296e5bb2a4f135ed7870da5e6fee742f3b
vvarona <vvaronad@gmail.com>
2022-06-27 11:30:44 +0200

    Day 3 of pair programing, conditionals

--- js-adakitten/js/main.js
+++ js-adakitten/js/main.js
@@ -2,8 +2,8 @@
 
 console.log('holis');
 
-const newForm = document.querySelector('.js_new-form');
-newForm.classList.remove('collapsed');
+
+
 
 const dataList = document.querySelector('.js-list');
 
@@ -12,9 +12,15 @@ Repetir este proceso por cada gatito */
 
 const kittenImage = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 const kittenName = "Anastacio";
-const kittenDesc =
+const kittenDesc1 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace = "British Shorthair";
+
+const kittenDesc2 =
+  "cariñoso, acariciarle!";
+
+  const kittenDesc3 =
+  "Ruiseño, cariñoso";
 
 const kittenOne = `<li class="card">
 <article>
@@ -26,7 +32,7 @@ const kittenOne = `<li class="card">
   <h3 class="card_title">${kittenName}</h3>
   <h4 class="card_race">${kittenRace}</h4>
   <p class="card_description">
-    ${kittenDesc}
+    ${kittenDesc2}
   </p>
 </article>
 </li>`;
@@ -63,6 +69,34 @@ const kittenThree = `<li class="card">
 </li>`
 
 
-dataList.innerHTML = kittenOne;
-dataList.innerHTML += kittenTwo;
-dataList.innerHTML += kittenThree;
+ 
+
+
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
+
+if( kittenDesc1.includes(descrSearchText) ) {
+ dataList.innerHTML += kittenOne;
+  }
+  
+  if( kittenDesc3.includes(descrSearchText) ) {
+  //Completa el código
+  dataList.innerHTML += kittenTwo;
+  }
+  
+  if( kittenDesc3.includes(descrSearchText) ) {
+  //Completa el código
+  dataList.innerHTML += kittenThree;
+  }
+  else {
+    dataList.innerHTML = 'No hay ningun gatito con ese atributo :( '
+  }
+
+
+  const newForm = document.querySelector('.js_new-form');
+
+  if (newForm.classList.contains('collapsed')) {
+   newForm.classList.remove('collapsed');
+  } 
